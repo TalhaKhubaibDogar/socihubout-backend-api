83a7331cbdc6dea707d5fcae026ef95db130e933
TalhaKhubaibDogar <talhakhubaib56@gmail.com>
2024-06-11 21:56:28 +0500

    custom message create for more dry code

---
+++ common/messages.py
@@ -0,0 +1,7 @@
+from django.utils.translation import gettext_lazy as _
+
+OTP_SENT = _("OTP sent to your Email")
+OTP_RESENT = _("OTP Resent to your Email")
+USER_VERIFIED = _("User Verified Successfully!")
+LOGIN_SUCCESS = _('You are logged in')
+LOGOUT_SUCCESS = _('Logged out successfully')
