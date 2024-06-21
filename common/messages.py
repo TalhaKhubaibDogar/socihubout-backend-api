da6b541d9109c38ae8ee275d9cc227d96dd34068
TalhaKhubaibDogar <talhakhubaib56@gmail.com>
2024-06-22 01:30:40 +0500

    messages auth

--- common/messages.py
+++ common/messages.py
@@ -3,5 +3,9 @@ from django.utils.translation import gettext_lazy as _
 OTP_SENT = _("OTP sent to your Email")
 OTP_RESENT = _("OTP Resent to your Email")
 USER_VERIFIED = _("User Verified Successfully!")
-LOGIN_SUCCESS = _('You are logged in')
-LOGOUT_SUCCESS = _('Logged out successfully')
+LOGIN_SUCCESS = _('You are logged in Successfully')
+LOGOUT_SUCCESS = _('Logged out Successfully')
+OTP_EMAIL_SUBJECT = _('Thanks for SignUp On SociHubOut')
+USER_EXISTS = _("Account Already Exists! Please login")
+
+INVALID_OTP = _("Your OTP is Invalid!")
